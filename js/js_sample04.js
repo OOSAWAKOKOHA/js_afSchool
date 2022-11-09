c84f0bd64fae97150ec7461cdf61d71a2a0182a5
tw234-jpg <103188290+tw234-jpg@users.noreply.github.com>
2022-11-09 20:02:01 +0900

    吹き出しと文字を表示

--- js/js_sample04.js
+++ js/js_sample04.js
@@ -2,8 +2,18 @@ const txtHall = document.getElementById("txtHall");
 const inTxt = document.getElementById("inTxt");
 const send = document.getElementById("send");
 const clear = document.getElementById("clear");
+const balloon = document.getElementById("balloon");
+const balloonNo = Math.floor(Math.random * balloon.length)
 
-
+// inTxt.addEventListener("")
 send.addEventListener("click",() => {
   console.log( inTxt.value );
+  //加算代入
+  txtHall.innerHTML += `<div id="balloon" class="balloon">${inTxt.value}</div>`;
 });
+
+clear.addEventListener("click",() => {
+  console.log( txtHall );
+  // txtHall.innerHTML -= `<div id="balloon" class="balloon">${inTxt.value}</div>`;
+  txtHall.innerHTML = "";
+})
