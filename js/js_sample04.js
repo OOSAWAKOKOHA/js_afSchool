f66e2cffe2fc6c27ee3f7414bf43262552c02efa
tw234-jpg <103188290+tw234-jpg@users.noreply.github.com>
2022-11-10 17:36:57 +0900

    授業で作成

--- js/js_sample04.js
+++ js/js_sample04.js
@@ -3,13 +3,22 @@ const inTxt = document.getElementById("inTxt");
 const send = document.getElementById("send");
 const clear = document.getElementById("clear");
 const balloon = document.getElementById("balloon");
-const balloonNo = Math.floor(Math.random * balloon.length)
+// const balloonNo = Math.floor(Math.random * balloon.length)
+
+const animation = 
 
 // inTxt.addEventListener("")
 send.addEventListener("click",() => {
-  console.log( inTxt.value );
-  //加算代入
-  txtHall.innerHTML += `<div id="balloon" class="balloon">${inTxt.value}</div>`;
+  // console.log( inTxt.value );
+  const randomX = Math.floor(Math.random() * (txtHall.clientWidth - 100));
+  const randomY = Math.floor(Math.random() * (txtHall.clientHeight - 50));
+
+  if (inTxt.value < 1){
+    alert( "error!!" )
+  }else{
+    //加算代入
+    txtHall.innerHTML += `<div id="balloon" class="balloon" style="top: ${randomY }px; left: ${randomX }px; animation: zoom-in-anim 0.3s alternate;">${inTxt.value}</div>`;
+  }
 });
 
 clear.addEventListener("click",() => {
